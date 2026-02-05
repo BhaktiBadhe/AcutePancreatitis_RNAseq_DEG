d475d2bfdd2a52d89ad6e0d405c9fca0539ed078
Bhakti Badhe <bhakti022003@gmail.com>
2026-02-05 13:19:15 +0530

    initial commit

--- AP_deg_pipeline.R
+++ AP_deg_pipeline.R
@@ -74,4 +74,83 @@ nrow(sig_deg)
 # Save results
 write.csv(sig_deg,"AP_vs_HC_DEGs.csv",row.names = FALSE)
 
+# ----- Visualization
+theme_bold <- function(base_size = 16) {
+  theme_classic(base_size = base_size) +
+    theme(
+      axis.title.x = element_text(face = "bold", size = base_size, color = "black"),
+      axis.title.y = element_text(face = "bold", size = base_size, color = "black"),
+      axis.text.x  = element_text(face = "bold", size = base_size - 2, color = "black"),
+      axis.text.y  = element_text(face = "bold", size = base_size - 2, color = "black"),
+      
+      plot.title   = element_text(face = "bold", size = base_size + 2, hjust = 0.5),
+      
+      legend.title = element_text(face = "bold", size = base_size - 2),
+      legend.text  = element_text(size = base_size - 3, face = "bold"),
+      legend.position = "right")
+}
+
+# PCA plot
+# Variance stabilizing transformation
+vsd <- vst(dds, blind = FALSE)
+
+# PCA
+ggplot(pca_df, aes(PC1, PC2, color = group2)) +
+  geom_point(size = 4) +
+  xlab(paste0("PC1 (", percentVar[1], "% variance)")) +
+  ylab(paste0("PC2 (", percentVar[2], "% variance)")) +
+  ggtitle("Acute Pancreatitis vs Healthy Controls") +
+  guides(color = guide_legend(title = "Group")) +
+  theme_bold()
+
+# Volcano plot
+volcano_df <- res_deg_df %>%
+  mutate(sig = case_when(
+      padj < 0.05 & log2FoldChange > 1  ~ "Upregulated",
+      padj < 0.05 & log2FoldChange < -1 ~ "Downregulated",
+      TRUE ~ "Not significant"))
+
+ggplot(volcano_df, aes(log2FoldChange, -log10(padj), color = sig)) +
+  geom_point(alpha = 0.6, size = 1.5) +
+  scale_color_manual(values = c(
+    "Upregulated" = "red",
+    "Downregulated" = "blue",
+    "Not significant" = "grey")) +
+  geom_vline(xintercept = c(-1, 1), linetype = "dashed") +
+  geom_hline(yintercept = -log10(0.05), linetype = "dashed") +
+  theme_classic() +
+  labs(title = "AP vs HC",
+       x = "log2 Fold Change",
+       y = "-log10 adjusted P-value") + theme_bold()
+
+# Heatmap for top DEGs
+# Select top 10 DEGs
+top_genes <- sig_deg %>%
+  slice_head(n = 10) %>%
+  pull(gene)
+
+# Extract normalized expression
+mat <- assay(vsd)[top_genes, ]
+
+# Scale by gene
+mat_scaled <- t(scale(t(mat)))
+
+# Annotation
+annotation_col <- data.frame(Group = colData(dds)$group2)
+rownames(annotation_col) <- colnames(mat_scaled)
+
+# Heatmap
+pheatmap( mat_scaled,
+  annotation_col = annotation_col,
+  show_colnames = FALSE,
+  fontsize_row = 8,
+  main = "Top 10 DEGs") + theme_bold()
+
+
+
+
+
+
+
+
 
